49abb4e5ecca6df2a2efd1b6327c9fbf3d0ac6fe
Frederik Jensen <frederikhj0@gmail.com>
2020-11-21 14:45:12 +0100

    Played around with positioning

--- src/main/kotlin/view/Main.kt
+++ src/main/kotlin/view/Main.kt
@@ -3,13 +3,21 @@ package view
 import tornadofx.*
 
 class Main: View() {
+    var lab = label()
     override val root = vbox {
         prefHeight = 500.0
         prefWidth = 500.0
 
-        label {
-            text = "Hello world!"
-            
+        lab = label {
+            text = "Hello World"
         }
+
+
+
+    }
+
+    override fun onDock() {
+        lab.translateX = ((root.width / 2) - (lab.width / 2))
+        println(lab.width)
     }
 }
