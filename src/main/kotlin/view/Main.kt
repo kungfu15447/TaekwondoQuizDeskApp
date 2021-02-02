b38101c02f7f2a3bb00b6001228da007ee4e463e
Frederik Jensen <frederikhj0@gmail.com>
2021-02-02 18:17:20 +0100

    Added two comboboxes
    They define what grade you have/are going to have (Still need to work on that) and how many questions you want

--- src/main/kotlin/view/Main.kt
+++ src/main/kotlin/view/Main.kt
@@ -1,9 +1,29 @@
 package view
 
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import tornadofx.*
 
 class Main: View() {
     var lab = label()
+    var gradeComboBox = combobox<String>()
+    var amountOfQComboBox = combobox<Number>()
+    var startQuizButton = button()
+    var beltGrades: ObservableList<String> = FXCollections.observableArrayList<String>(
+            "10. Kup",
+            "9. Kup",
+            "8. Kup",
+            "7. Kup"
+    )
+    var questionAmount: ObservableList<Number> = FXCollections.observableArrayList<Number>(
+            10,
+            20,
+            30
+    )
+    var selectedGrade = SimpleStringProperty()
+    var selectedAmount = SimpleIntegerProperty()
     override val root = vbox {
         prefHeight = 500.0
         prefWidth = 500.0
@@ -12,12 +32,40 @@ class Main: View() {
             text = "Hello World"
         }
 
+        gradeComboBox = combobox {
+            bindSelected(selectedGrade)
+            value = beltGrades[0]
+            items = beltGrades
+        }
 
+        amountOfQComboBox = combobox {
+            label {
+                text = "Spørgsmål: "
+            }
+            bindSelected(selectedAmount)
+            value = questionAmount[0]
+            items = questionAmount
+        }
+
+        startQuizButton = button {
+            text = "Start quiz!"
+            action {
+                println("Du har nu startet quizzen. ER DU FORBEREDT??")
+            }
+        }
+    }
+
+    init {
+        selectedGrade.onChange {
+            println("Selected grade: " + selectedGrade.value)
+        }
+        selectedAmount.onChange {
+            println("Selected amount " + selectedAmount.value)
+        }
 
     }
 
     override fun onDock() {
         lab.translateX = ((root.width / 2) - (lab.width / 2))
-        println(lab.width)
     }
 }
